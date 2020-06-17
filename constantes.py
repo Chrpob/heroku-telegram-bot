7b7215714b1caad3f7d722362b475815a727c50c
Chrpob <chrpob@gmail.com>
2020-06-17 02:20:47 -0400

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,7 +1,7 @@
 TEXTO_PRINCIPAL = '¿Qué es?\n FonoaudiologaBot es una robot que colabora optimiznado el tiempo en el cálculo de desviaciones ' \
                   'estándar, percentiles y puntajes en distintas pruebas.' \
-                  
-                  'Puedes apoyar el desarrollo comprando un café en: https://www.buymeacoffee.com/presionaenter\n' \
+                  'Puedes apoyar el desarrollo comprando un café en: https://www.buymeacoffee.com/presionaenter' \
+
                    'Las pruebas incluidas y comandos son:\n\n' \
                   
                   '/edna para calcular EDNA\n' \
